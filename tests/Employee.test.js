6730a2391774cd657b24eb8c4e13d8a7a4f2860f
Julian Almendarez <ppalmendarez@yahoo.com>
2021-04-11 19:37:38 -0500

    set up classes

---
+++ tests/Employee.test.js
@@ -0,0 +1,50 @@
+const Employee = require("../lib/Employee")
+
+describe('Employee', () =>{
+
+    // name
+    it('should return..', () => {
+
+        let employeeName = "";
+        let newEmployee = new Employee("");
+        expect(newEmployee.name).toBe("");
+        
+    });
+
+    // id
+    it('should return..', () => {
+
+        let employeeName = "";
+        let newEmployee = new Employee("");
+        expect(newEmployee.name).toBe("");
+        
+    });
+
+    // email
+    it('should return..', () => {
+
+        let employeeName = "";
+        let newEmployee = new Employee("");
+        expect(newEmployee.name).toBe("");
+        
+    });
+
+})  
+
+getName(){
+
+}
+
+getId(){
+
+}
+
+getEmail(){
+
+}
+
+getRole(){
+    return 'Employee'
+} 
+
+
